7276e598dd4f9e5cf03a68c27c1caa22d7132c30
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2018-01-16 20:02:17 +0100

    cleanup

--- src/main/java/com/controller/api/ArticleApi.java
+++ src/main/java/com/controller/api/ArticleApi.java
@@ -1,13 +1,11 @@
 package com.controller.api;
 
-import com.controller.transfer.converter.ArticleToArticleDtoConverter;
-import com.repository.ArticleRepository;
 import com.controller.transfer.dto.ArticleDto;
+import com.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.controller.transfer.converter.ArticleToArticleDtoConverter.convert;
--- src/main/java/com/repository/ArticleRepository.java
+++ src/main/java/com/repository/ArticleRepository.java
@@ -4,8 +4,6 @@ import com.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public interface ArticleRepository extends CrudRepository<Article, String> {
 }
