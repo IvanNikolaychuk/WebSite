28037ea237d099994ff2b00d689556652e8d519c
Ivan Nikolaichuk <ivan_nikolaichuk@epam.com>
2018-01-17 07:33:12 +0100

    Quering article previews on home page load

--- src/main/java/com/controller/transfer/dto/ArticleDto.java
+++ src/main/java/com/controller/transfer/dto/ArticleDto.java
@@ -2,19 +2,26 @@ package com.controller.transfer.dto;
 
 import com.entity.Article;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 public class ArticleDto {
+    private static final String DATE_FORMAT_PATTERN = "dd-MM-yyyy";
+
     public String preview;
     public String name;
     public List<String> tags;
-    public Date creationDate;
+    public String creationDate;
 
     public ArticleDto(Article article) {
         this.preview = article.getPreview();
         this.name = article.getName();
         this.tags = article.getTags();
-        this.creationDate = article.getCreationDate();
+        this.creationDate = toString(article.getCreationDate());
+    }
+
+    private String toString(Date creationDate) {
+        return new SimpleDateFormat(DATE_FORMAT_PATTERN).format(creationDate);
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,30 @@
+$(function () {
+    function createTagsHtml(tags) {
+        var html = '';
+        for (var tagIndex = 0; tagIndex < tags.length; tagIndex++) {
+            html += '                   <div class="tag">' + tags[tagIndex] + '</div>';
+        }
+
+        return html;
+    }
+
+    $.get("/api/articles/", function (data) {
+        for (var i = 0; i < data.length; i++) {
+            var article = data[i];
+            var html =
+                '<div class="topic">' +
+                '      <div class="title">' +
+                '           <div class="article-name">' + article.name + '</div>' +
+                '               <div class="tags">' + createTagsHtml(article.tags) + '</div>' +
+                '            <div class="time">' + article.creationDate + '</div>' +
+                '      </div>' +
+                '      <div class="topic-content">' + article.preview + '</div>' +
+                '      <div class="read-all-btn">' +
+                '           <div class="read-fully">Read</div>' +
+                '      </div>' +
+                '</div>';
+            $("#topic-wrapper")
+                .append($(html));
+        }
+    });
+});
